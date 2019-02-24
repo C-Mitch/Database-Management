df4361c8ff03e292fef7fe5f3e4e8caef7686b25
Chase Mitchell <C-Mitch@users.noreply.github.com>
2019-02-24 14:14:17 -0800

    Updated SimpleDB.py

--- SimpleDB.py
+++ SimpleDB.py
@@ -1,1 +1,22 @@
-TEst
+#Chase Mitchell
+#002274657
+#Mitch213@mail.chapman.edu
+#CPSC-408-01
+#Assignment2
+
+import sqlite3
+conn = sqlite3.connect('StudentDB.db')
+c = conn.cursor()
+
+c.execute('CREATE TABLE Student (
+            StudentId PK INT,
+            FirstName varchar(25),
+            LastName varchar(25),
+            GPA Numeric,
+            Major varchar(10),
+            FacultyAdvisor varchar(25)
+            )')
+
+conn.commit()
+
+conn.close()
