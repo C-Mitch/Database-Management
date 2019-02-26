9b2981c9698746e943785f90cadc3a6f14e5056e
Chase Mitchell <C-Mitch@users.noreply.github.com>
2019-02-25 18:21:03 -0800

    Added Menu.py & gitignore

---
+++ Menu.py
@@ -0,0 +1,31 @@
+#Chase Mitchell
+#002274657
+#Mitch213@mail.chapman.edu
+#CPSC-408-01
+#Assignment2
+
+import sqlite3
+
+def getAll(x):
+    c.execute("SELECT * FROM Student")
+    result = c.fetchall()
+    for x in result:
+        print(x)
+
+def getMajor(x):
+    c.execute("SELECT FirstName, LastName, Major FROM Student WHERE Major = 'CPSC'")
+    result = c.fetchall()
+    for x in result:
+        print(x)
+
+def getGPA(x):
+    c.execute("SELECT FirstName, LastName, GPA FROM Student WHERE GPA >= ?", (3.5, ))
+    result = c.fetchall()
+    for x in result:
+        print(x)
+
+def getAdvisor(x):
+    c.execute("SELECT FirstName, LastName, FacultyAdvisor FROM Student WHERE FacultyAdvisor = 'Rene'")
+    result = c.fetchall()
+    for x in result:
+        print(x)
--- SimpleDB.py
+++ SimpleDB.py
@@ -4,31 +4,7 @@
 #CPSC-408-01
 #Assignment2
 
-import sqlite3
-
-def getAll(x):
-    c.execute("SELECT * FROM Student")
-    result = c.fetchall()
-    for x in result:
-        print(x)
-
-def getMajor(x):
-    c.execute("SELECT FirstName, LastName, Major FROM Student WHERE Major = 'CPSC'")
-    result = c.fetchall()
-    for x in result:
-        print(x)
-
-def getGPA(x):
-    c.execute("SELECT FirstName, LastName, GPA FROM Student WHERE GPA >= 3.5")
-    result = c.fetchall()
-    for x in result:
-        print(x)
-
-def getAdvisor(x):
-    c.execute("SELECT FirstName, LastName, FacultyAdvisor FROM Student WHERE FacultyAdvisor = 'Rene'")
-    result = c.fetchall()
-    for x in result:
-        print(x)
+from Menu import Menu
 
 conn = sqlite3.connect('StudentDB.db')
 c = conn.cursor()
