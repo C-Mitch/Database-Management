2dce87aad41f5b32eae4e7d179d2f660ababba21
C-Mitch <C-Mitch@users.noreply.github.com>
2019-04-12 18:36:01 -0700

    Added Assignment 3 && FakerGeneration.py

---
+++ Assignment 3/FakerGeneration.py
@@ -0,0 +1,43 @@
+#Chase Mitchell
+#002274657
+#Mitch213@mail.chapman.edu
+#CPSC-408-01
+#Assignment3
+
+import sys
+import csv
+from faker import Faker
+
+if len(sys.argv) == 3:
+	if sys.argv[2].isdigit():
+		rows = int(sys.argv[2])
+		with open(sys.argv[1], 'w') as csvFile:
+			faker = Faker()
+			columns = ['FirstName', 'LastName', 'DateOfBirth', 'SSN',
+			'Address', 'Email', 'Country', 'Job', 'UserName', 'Password']
+			csvWriter = csv.DictWriter(csvFile, fieldnames = columns)
+			
+			csvWriter.writeheader()
+			while rows > 0:
+			
+				csvWriter.writerow({
+                'FirstName': faker.first_name(),
+                'LastName': faker.last_name(),
+				'DateOfBirth': faker.date(pattern="%Y-%m-%d", end_datetime="-10y"),
+                'SSN': faker.itin(),
+                'Address': faker.street_address(),
+				'Email': faker.email(),
+                'Country': faker.country(),
+                'Job': faker.job().replace(",", ''),
+                'UserName': faker.user_name(),
+                'Password': faker.password()
+                })
+				
+				rows -= 1
+				
+		print("File Generated Successfully")
+	else:
+		print("Please Pass An Integer Value As The Second Argument At Execution")
+
+else:
+	print("Please A File Name And Number Of Rows To Be Generated At Execution")
